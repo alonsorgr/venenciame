16e7bd12b02edf8915caadad31ab82bc3853d3af
Alonso <alonso.garcia@iesdonana.org>
2020-08-11 12:01:17 +0200

    minor(nombre-aplicación): establecido el nombre final de la aplicación

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $log = require __DIR__ . '/log.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'Venénciame',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
