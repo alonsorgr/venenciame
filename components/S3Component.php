ba3aa1d75238c4068fb73d44bfe858f25f72e343
Alonso <alonso.garcia@iesdonana.org>
2020-08-20 15:49:11 +0200

    docs(S3Component): agregada documentación

--- components/S3Component.php
+++ components/S3Component.php
@@ -1,29 +1,77 @@
 <?php
 
+/**
+ * @link https://github.com/alonsorgr/venenciame/
+ * @copyright Copyright (c) 2020 alonsorgr
+ * @license https://github.com/alonsorgr/venenciame/blob/master/LICENSE.md
+ */
+
 namespace app\components;
 
 use Aws\S3\S3Client;
 use yii\base\Component;
 
-
+/**
+ * Componente de aplicación para la administración de imágenes en AWS S3.
+ *
+ * @author Alonso García <alonsorgr@gmail.com>
+ * @since 1.0
+ */
 class S3Component extends Component
 {
+    /**
+     * Variable para la versión de AWS S3.
+     *
+     * @var string
+     */
     public $version;
 
+    /**
+     * Variable para la región de AWS S3.
+     *
+     * @var string
+     */
     public $region;
 
+    /**
+     * Variable para la clave de AWS S3.
+     *
+     * @var string
+     */
     public $key;
 
+    /**
+     * Variable para la clave secreta de AWS S3.
+     *
+     * @var string
+     */
     public $secret;
 
+    /**
+     * Variable para el cliente de AWS S3.
+     *
+     * @var \Aws\S3\S3Client
+     */
     public $s3Client;
 
+    /**
+     * Constructor del componente AWS S3.
+     *
+     * @param array $config configuración del componente.
+     */
     public function __construct($config = [])
     {
         $this->s3Client($config, $this->s3Client);
         parent::__construct($config);
     }
 
+    /**
+     * Establece las propiedades del componente [[Aws\S3\S3Client]].
+     *
+     * @param   array               $config     configuración del componente.
+     * @param   \Aws\S3\S3Client    $s3Client   cliente AWS S3.
+     * @return  void
+     */
     private function s3Client($config, &$s3Client)
     {
         $s3Client = new S3Client([
@@ -36,6 +84,14 @@ class S3Component extends Component
         ]);
     }
 
+    /**
+     * Subida de imágenes a AWS S3.
+     *
+     * @param   string    $bucket    nombre del bucket.
+     * @param   string    $key       nombre de la imagen.
+     * @param   string    $body      imagen subir.
+     * @return  string               nombre de la imagen subida.
+     */
     public function upload($bucket, $key, $body)
     {
         $this->s3Client->putObject([
@@ -47,6 +103,13 @@ class S3Component extends Component
         return $key;
     }
 
+    /**
+     * Bajada de imágenes a AWS S3.
+     *
+     * @param   string          $bucket    nombre del bucket.
+     * @param   string          $key       nombre de la imagen.
+     * @return  \Aws\Result                imagen de AWS S3.
+     */
     public function download($bucket, $key)
     {
         return $this->s3Client->getObject([
@@ -55,6 +118,13 @@ class S3Component extends Component
         ]);
     }
 
+    /**
+     * Elimina una imagen de AWS S3.
+     *
+     * @param   string          $bucket    nombre del bucket.
+     * @param   string          $key       nombre de la imagen.
+     * @return  \Aws\Result                resultado de la eliminación de la imagen.
+     */
     public function delete($bucket, $key)
     {
         $this->s3Client->deleteObject([
@@ -63,7 +133,14 @@ class S3Component extends Component
         ]);
     }
 
-    public function getUrl($key, $bucket)
+    /**
+     * Obtiene la url de un fichero de AWS S3.
+     *
+     * @param   string $bucket    nombre del bucket.
+     * @param   string $key       nombre de la imagen.
+     * @return  string            url de la imagen.
+     */
+    public function getUrl($bucket, $key)
     {
         return $this->s3Client->getObjectUrl($bucket, $key);
     }
