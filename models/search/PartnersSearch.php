29a9770263ffc77ec213b21e4fff8f13cd5b5166
Alonso <alonso.garcia@iesdonana.org>
2020-08-22 17:13:08 +0200

    feat(partners): generación del CRUD de socios

---
+++ models/search/PartnersSearch.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\models\search;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Partners;
+
+/**
+ * PartnersSearch represents the model behind the search form of `app\models\Partners`.
+ */
+class PartnersSearch extends Partners
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'user_id', 'country_id', 'state_id'], 'integer'],
+            [['name', 'description', 'information', 'image', 'city', 'zip_code', 'address', 'phone', 'updated_at', 'created_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Partners::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'country_id' => $this->country_id,
+            'state_id' => $this->state_id,
+            'updated_at' => $this->updated_at,
+            'created_at' => $this->created_at,
+        ]);
+
+        $query->andFilterWhere(['ilike', 'name', $this->name])
+            ->andFilterWhere(['ilike', 'description', $this->description])
+            ->andFilterWhere(['ilike', 'information', $this->information])
+            ->andFilterWhere(['ilike', 'image', $this->image])
+            ->andFilterWhere(['ilike', 'city', $this->city])
+            ->andFilterWhere(['ilike', 'zip_code', $this->zip_code])
+            ->andFilterWhere(['ilike', 'address', $this->address])
+            ->andFilterWhere(['ilike', 'phone', $this->phone]);
+
+        return $dataProvider;
+    }
+}
---
+++ views/partners/_search.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\bootstrap4\Html;
+use yii\bootstrap4\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\search\PartnersSearch */
+/* @var $form yii\bootstrap4\ActiveForm */
+?>
+
+<div class="partners-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'user_id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?= $form->field($model, 'information') ?>
+
+    <?php // echo $form->field($model, 'image') ?>
+
+    <?php // echo $form->field($model, 'country_id') ?>
+
+    <?php // echo $form->field($model, 'state_id') ?>
+
+    <?php // echo $form->field($model, 'city') ?>
+
+    <?php // echo $form->field($model, 'zip_code') ?>
+
+    <?php // echo $form->field($model, 'address') ?>
+
+    <?php // echo $form->field($model, 'phone') ?>
+
+    <?php // echo $form->field($model, 'updated_at') ?>
+
+    <?php // echo $form->field($model, 'created_at') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
