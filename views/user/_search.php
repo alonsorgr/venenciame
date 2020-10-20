046171443102dac93103c2fe5da6874ea22358ce
Alonso <alonso.garcia@iesdonana.org>
2020-10-20 21:41:21 +0200

    feat:(admin): nuevo diseño de vista de usuarios en panel de administración

--- views/user/_search.php
+++ views/user/_search.php
@@ -1,5 +1,10 @@
 <?php
 
+use app\models\Roles;
+use app\models\Statuses;
+use kartik\date\DatePicker;
+use kartik\datecontrol\DateControl;
+use kartik\select2\Select2;
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
 
@@ -17,46 +22,69 @@ use yii\bootstrap4\ActiveForm;
             'data-pjax' => 1
         ],
     ]); ?>
-
-    <?= $form->field($model, 'id') ?>
-
-    <?= $form->field($model, 'username') ?>
-
-    <?= $form->field($model, 'password') ?>
-
-    <?= $form->field($model, 'email') ?>
-
-    <?= $form->field($model, 'auth_key') ?>
-
-    <?php // echo $form->field($model, 'verf_key') ?>
-
-    <?php // echo $form->field($model, 'status') ?>
-
-    <?php // echo $form->field($model, 'admin')->checkbox() ?>
-
-    <?php // echo $form->field($model, 'privacity')->checkbox() ?>
-
-    <?php // echo $form->field($model, 'name') ?>
-
-    <?php // echo $form->field($model, 'surname') ?>
-
-    <?php // echo $form->field($model, 'birthdate') ?>
-
-    <?php // echo $form->field($model, 'image') ?>
-
-    <?php // echo $form->field($model, 'rol_id') ?>
-
-    <?php // echo $form->field($model, 'language_id') ?>
-
-    <?php // echo $form->field($model, 'updated_at') ?>
-
-    <?php // echo $form->field($model, 'created_at') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+    <div class="row">
+        <div class="col-12 col-md-2">
+            <?= $form->field($model, 'username')->textInput([
+                'maxlength' => true,
+                'placeholder' => Yii::t('app', 'Buscar por nombre de usuario'),
+                'title' => Yii::t('app', 'Nombre de usuario'),
+            ]); ?>
+        </div>
+        <div class="col-12 col-md-2">
+            <?= $form->field($model, 'email')->textInput([
+                'maxlength' => true,
+                'placeholder' => Yii::t('app', 'Buscar por correo electrónico'),
+                'title' => Yii::t('app', 'Correo electrónico'),
+            ]); ?>
+        </div>
+        <div class="col-12 col-md-2">
+            <?= $form->field($model, 'status_id')->widget(Select2::class, [
+                'data' => Statuses::labels(),
+                'options' => [
+                    'class' => 'form-control',
+                    'placeholder' => Yii::t('app', 'Buscar por estado'),
+                    'title' => Yii::t('app', 'Estado'),
+                ],
+                'theme' => Select2::THEME_MATERIAL,
+                'pluginOptions' => [
+                    'allowClear' => false,
+                ],
+            ]); ?>
+        </div>
+        <div class="col-12 col-md-2">
+            <?= $form->field($model, 'rol_id')->widget(Select2::class, [
+                'data' => Roles::labels(),
+                'options' => [
+                    'class' => 'form-control',
+                    'placeholder' => Yii::t('app', 'Buscar por rol de usuario'),
+                    'title' => Yii::t('app', 'Rol de usuario'),
+                ],
+                'theme' => Select2::THEME_MATERIAL,
+                'pluginOptions' => [
+                    'allowClear' => false,
+                ],
+            ]); ?>
+        </div>
+        <div class="col-12 col-md-2">
+            <?= $form->field($model, 'created_at')->widget(DateControl::class, [
+                'widgetOptions' => [
+                    'type' => DatePicker::TYPE_COMPONENT_APPEND,
+                    'pluginOptions' => [
+                        'autoclose' => true,
+                    ],
+                    'options' => [
+                        'placeholder' => Yii::t('app', 'Buscar por fecha'),
+                    ]
+                ],
+            ]); ?>
+        </div>
+        <div class="col-12 col-md-2 my-4">
+            <?= Html::submitButton('<i class="fas fa-search mr-2"></i>' . Yii::t('app', 'Buscar'), [
+                'class' => 'btn btn-outline-primary btn-block',
+                'placeholder' => Yii::t('app', 'Buscar'),
+                'title' => Yii::t('app', 'Buscar'),
+            ]); ?>
+        </div>
     </div>
-
     <?php ActiveForm::end(); ?>
-
 </div>
