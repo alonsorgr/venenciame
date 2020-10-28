0fa74503cb4695635de0d4625a7f7adb754a4446
Alonso <alonso.garcia@iesdonana.org>
2020-10-28 17:23:19 +0100

    feat(carrito): generado modelo

---
+++ models/CartItems.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cart_items".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $article_id
+ * @property int $status_id
+ * @property int $quantity
+ * @property string|null $created_at
+ *
+ * @property Articles $article
+ * @property Statuses $status
+ * @property Users $user
+ */
+class CartItems extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cart_items';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'article_id', 'status_id', 'quantity'], 'required'],
+            [['user_id', 'article_id', 'status_id', 'quantity'], 'default', 'value' => null],
+            [['user_id', 'article_id', 'status_id', 'quantity'], 'integer'],
+            [['created_at'], 'safe'],
+            [['user_id', 'article_id'], 'unique', 'targetAttribute' => ['user_id', 'article_id']],
+            [['article_id'], 'exist', 'skipOnError' => true, 'targetClass' => Articles::className(), 'targetAttribute' => ['article_id' => 'id']],
+            [['status_id'], 'exist', 'skipOnError' => true, 'targetClass' => Statuses::className(), 'targetAttribute' => ['status_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => Users::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'article_id' => Yii::t('app', 'Article ID'),
+            'status_id' => Yii::t('app', 'Status ID'),
+            'quantity' => Yii::t('app', 'Quantity'),
+            'created_at' => Yii::t('app', 'Created At'),
+        ];
+    }
+
+    /**
+     * Gets query for [[Article]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticle()
+    {
+        return $this->hasOne(Articles::className(), ['id' => 'article_id'])->inverseOf('cartItems');
+    }
+
+    /**
+     * Gets query for [[Status]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStatus()
+    {
+        return $this->hasOne(Statuses::className(), ['id' => 'status_id'])->inverseOf('cartItems');
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(Users::className(), ['id' => 'user_id'])->inverseOf('cartItems');
+    }
+}
