9a296dd1061c94449c43ec146d150dc9e79965a8
Alonso <alonso.garcia@iesdonana.org>
2020-10-30 13:46:50 +0100

    fix: corregidos fallos de verificación

--- config/web.php
+++ config/web.php
@@ -74,13 +74,6 @@ $config = [
         'formatter' => [
             'timeZone' => 'Europe/Madrid',
         ],
-        'assetManager' => [
-            'bundles' => [
-                'kartik\form\ActiveFormAsset' => [
-                    'bsDependencyEnabled' => false // do not load bootstrap assets for a specific asset bundle
-                ],
-            ],
-        ],
         
         /* 'urlManager' => [
             'enablePrettyUrl' => true,
