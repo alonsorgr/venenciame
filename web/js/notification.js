aee4103c57a5c19365d143aed0cfca3fa662e2a4
Alonso <alonso.garcia@iesdonana.org>
2020-11-04 11:31:29 +0100

    feat: nuevo diseño de notificación

--- web/js/notification.js
+++ web/js/notification.js
@@ -6,7 +6,19 @@
  * @param string position   posición de la notificación.
  * @param string style      estilo de la notificación.
  */
-function notification(id, text, type, position = 'bottom left', style = 'bootstrap') {
+function notification(id, text, type, position = 'bottom left', style = 'app') {
+    $.notify.addStyle('app', {
+        html: "<div style='z-index: 99999'><i class='fas fa-check-circle mr-2' style='color: #797979'></i><span data-notify-text/></div>",
+        classes: {
+            base: {
+                "white-space": "nowrap",
+                "background-color": "#F2F2F2",
+                "border-radius": "4px",
+                "padding": "10px",
+                "border": "1px solid #d9d9d9",
+            },
+        }
+    });
     $(id).notify(text, {
         style: style,
         className: type,
