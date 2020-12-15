f105fede56cfc633b2bdf8d2a44898e68719fddd
Alonso García <alonso.garcia@iesdonana.org>
2020-12-15 10:19:13 +0100

    feat: se añade controlador para repartidores

---
+++ controllers/DealerController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class DealerController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
