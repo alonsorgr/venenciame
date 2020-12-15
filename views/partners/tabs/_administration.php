461e7d7e75353561f9a1a88fc987cabfae398bc7
Alonso García <alonso.garcia@iesdonana.org>
2020-12-15 09:56:53 +0100

    fix: tamaño botones en vista socios

--- views/partners/tabs/_administration.php
+++ views/partners/tabs/_administration.php
@@ -14,13 +14,13 @@ use yii\widgets\Pjax;
 
 <div class="partners-administration">
     <div class="row">
-        <div class="col-xl-6">
+        <div class="col-xl-3">
             <?= Html::a('<i class="fas fa-edit mr-md-2"></i>' . Yii::t('app', 'Editar información'), ['/partners/update', 'id' => $model->id], [
                 'class' => 'btn btn-outline-primary btn-block mt-5',
                 'title' => Yii::t('app', 'Editar información'),
             ]); ?>
         </div>
-        <div class="col-xl-6">
+        <div class="col-xl-3">
             <?= Html::a('<i class="fas fa-plus mr-md-2"></i>' . Yii::t('app', 'Agregar nuevo producto'), ['/articles/create'], [
                 'class' => 'btn btn-outline-primary btn-block mt-5',
                 'title' => Yii::t('app', 'Agregar nuevo producto'),
